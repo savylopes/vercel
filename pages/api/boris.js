0ada5ac07933ea2856f0d435790771e78d5aed5b
Boris Lopes <savylopes@gmail.com>
2020-09-07 13:16:05 -0100

    second

--- pages/api/boris.js
+++ pages/api/boris.js
@@ -1,8 +1,8 @@
 export default (req,res) =>{
     res.statusCode = 200
     res.json({
-        name:"Boris Lopes",
-        birthDay: "15/11/1990",
-        motherName: "Canuta Lopes"
+        name:"test name",
+        birthDay: "92103131",
+        motherName: "nome mãe"
     })
 }
